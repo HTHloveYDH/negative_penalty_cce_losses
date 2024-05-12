d0a3a718ec0db57d2a822ef951ac836170eecc17
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-05-12 22:08:32 +0800

    Update negative_penalty_focal_ce_losses.py

--- pytorch_version/losses/negative_penalty_focal_ce_losses.py
+++ pytorch_version/losses/negative_penalty_focal_ce_losses.py
@@ -60,7 +60,7 @@ class NegativePenaltySparseCategoricalFocalCrossentropy(nn.Module):
 
     def forward(self, y_pred, y_true):
         num_classes = y_pred.shape[-1]
-        # y_true = torch.squeeze(F.one_hot(y_true, num_classes=num_classes), dim=1)
+        # y_true = torch.squeeze(F.one_hot(y_true, num_classes=num_classes).float(), dim=1)
         y_true = F.one_hot(y_true, num_classes=num_classes).float()
         losses = _get_losses(
             y_true, y_pred, self.p_indices, self.penalty_label, self.alpha, self.alpha_l, self.gamma, 
